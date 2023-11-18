47ec60b87a67190a256b69a18bc9c4e7bd55ce9e
Ivan Gordillo <gordillosolis@icloud.com>
2023-11-17 18:32:14 -0600

    init

---
+++ lab01a/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Escribe una función en C++ que tome un arreglo de valores int y determine si hay un par de números en el arreglo cuyo producto es par.
+*/
+
+int main() {
+    int total
+}
